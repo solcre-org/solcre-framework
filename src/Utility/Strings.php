66c1ca8180b7e063018ce909c5311a20c07cae4f
Diego <diego.r.izarrualde@gmail.com>
2019-11-12 14:34:11 -0300

    ga!
    t Pleas enter the commit message for your changes. Lines starting

--- src/Utility/Strings.php
+++ src/Utility/Strings.php
@@ -3,6 +3,7 @@
 namespace Solcre\SolcreFramework2\Utility;
 
 use ForceUTF8\Encoding;
+use Solcre\SolcreFramework2\Exception\StringException;
 
 class Strings
 {
@@ -14,12 +15,14 @@ class Strings
     {
         $salt = substr(str_replace('+', '.', base64_encode(sha1(microtime(true), true))), 0, 22);
         $hash = crypt($password, '$2a$' . $cost . '$' . $salt);
+
         return $hash;
     }
 
     public static function verifyBcryptPassword($password, $existingBscrypt)
     {
         $hash = crypt($password, $existingBscrypt);
+
         return ($hash === $existingBscrypt);
     }
 
@@ -30,24 +33,28 @@ class Strings
 
     public static function generateRandomPassword($lenght)
     {
-        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!#$%&/()?~[]";
-        $pass = []; //remember to declare $pass as an array
+        $alphabet    = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!#$%&/()?~[]";
+        $pass        = []; //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+
         for ($i = 0; $i < $lenght; $i++) {
-            $n = rand(0, $alphaLength);
+            $n      = rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }
+
         return implode($pass); //turn the array into a string
     }
 
     public static function generateAlphaNumericString($length = 8)
     {
         $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        $string = '';
-        $max = strlen($characters) - 1;
+        $string     = '';
+        $max        = strlen($characters) - 1;
+
         for ($i = 0; $i < $length; $i++) {
             $string .= $characters[mt_rand(0, $max)];
         }
+
         return strtoupper($string);
     }
 
@@ -67,9 +74,11 @@ class Strings
     public static function validateRut($rut)
     {
         $rut = strlen((string)$rut);
+
         if ($rut !== 12) {
             throw new \InvalidArgumentException('Rut param incorrect formatted', 422);
         }
+
         return true;
     }
 
@@ -100,17 +109,96 @@ class Strings
 
     public static function cleanName($text)
     {
-        $space_char = '-';
-        $text = self::strtolowerUtf8(trim(Encoding::toUTF8($text)));
-        $code_entities_match = ['¿', 'ñ', 'á', 'é', 'í', 'ó', 'ú', ' ', '--', '&quot;', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '{', '}', '|', ':', '"', '<', '>', '?', '[', ']', '\\', ';', "'", ',', '/', '*', '+', '~', '`', '=', '°', '´'];
-        $code_entities_replace = ['', 'n', 'a', 'e', 'i', 'o', 'u', $space_char, '_', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+        $space_char          = '-';
+        $text                = self::strtolowerUtf8(trim(Encoding::toUTF8($text)));
+        $code_entities_match = [
+            '¿',
+            'ñ',
+            'á',
+            'é',
+            'í',
+            'ó',
+            'ú',
+            ' ',
+            '--',
+            '&quot;',
+            '!',
+            '@',
+            '#',
+            '$',
+            '%',
+            '^',
+            '&',
+            '*',
+            '(',
+            ')',
+            '+',
+            '{',
+            '}',
+            '|',
+            ':',
+            '"',
+            '<',
+            '>',
+            '?',
+            '[',
+            ']',
+            '\\',
+            ';',
+            "'",
+            ',',
+            '/',
+            '*',
+            '+',
+            '~',
+            '`',
+            '=',
+            '°',
+            '´'
+        ];
+        $code_entities_replace = [
+            '',
+            'n',
+            'a',
+            'e',
+            'i',
+            'o',
+            'u',
+            $space_char,
+            '_',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
         $text = str_replace($code_entities_match, $code_entities_replace, $text);
+
         return str_replace('__', '_', $text);
     }
 
     public static function strtolowerUtf8($string)
     {
-        $string = trim(Encoding::toUTF8($string));
+        $string     = trim(Encoding::toUTF8($string));
         $convert_to = [
             "a",
             "b",
@@ -291,6 +379,7 @@ class Strings
             "Ю",
             "Я"
         ];
+
         return str_replace($convert_from, $convert_to, $string);
     }
 
@@ -301,24 +390,37 @@ class Strings
 
     public static function encrypt($stringToEncrypt, $validateKey = self::VALIDATE_KEY)
     {
+        if (openssl_cipher_iv_length(self::ENCRYPTION_METHOD) === false) {
+            throw StringException::openssl_cipher_iv_length_Exception();
+        }
+
         $cryptoStrong = true;
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::ENCRYPTION_METHOD), $cryptoStrong);
+        $iv           = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::ENCRYPTION_METHOD), $cryptoStrong);
+
         if (false === $cryptoStrong || false === $iv) {
             throw new \RuntimeException('IV generation failed');
         }
+
         $stringEncrypted = openssl_encrypt($stringToEncrypt, self::ENCRYPTION_METHOD, $validateKey, 0, $iv);
+
         return base64_encode($stringEncrypted . self::ENCRYPTION_GLUE . $iv);
     }
 
     public static function decrypt($encryptedString, $validateKey = self::VALIDATE_KEY)
     {
+        if (base64_decode($encryptedString) === false) {
+            throw StringException::base64_decode_Exception();
+        }
+
         list($encryptedString, $iv) = explode(self::ENCRYPTION_GLUE, base64_decode($encryptedString), 2);
+
         return openssl_decrypt($encryptedString, self::ENCRYPTION_METHOD, $validateKey, 0, $iv);
     }
 
     public static function replaceFirstOccurrence($haystack, $needle, $replace)
     {
         $pos = strpos($haystack, $needle);
+
         if ($pos !== false && $pos === 0) {
             return substr_replace($haystack, $replace, $pos, strlen($needle));
         }
@@ -326,3 +428,4 @@ class Strings
         return null;
     }
 }
+
